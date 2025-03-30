c915773c1f29258d38ab880e743130249472895c
Ceshine Lee <shuanck@gmail.com>
2025-03-30 21:51:20 +0800

    Minor refactor

--- src/git_prompts_mcp_server/server.py
+++ src/git_prompts_mcp_server/server.py
@@ -55,6 +55,20 @@ def _format_diff_results_as_json(diff_results: list[git.Diff]) -> str:
     )
 
 
+def _get_diff_results(
+    repo: git.Repo, source_commit: git.Commit, target_commit: git.Commit, excludes: list[str]
+) -> list[git.Diff]:
+    diff_results = source_commit.diff(target_commit, create_patch=True)
+
+    for exclude_pattern in excludes:
+        diff_results = [
+            item
+            for item in diff_results
+            if not fnmatch(item.a_path or "", exclude_pattern) and not fnmatch(item.b_path or "", exclude_pattern)
+        ]
+    return diff_results
+
+
 async def run(repository: Path, excludes: list[str] = [], json_format: bool = True):
     logger = logging.getLogger(__name__)
 
@@ -81,34 +95,33 @@ async def run(repository: Path, excludes: list[str] = [], json_format: bool = Tr
             if not arguments:
                 raise ValueError("Arguments required")
 
-            source_commit = repo.commit(arguments.get("ancestor"))
-            target_commit = repo.commit(arguments.get("HEAD"))
-
-            diff_results = source_commit.diff(target_commit, create_patch=True)
-
-            for exclude_pattern in excludes:
-                diff_results = [
-                    item
-                    for item in diff_results
-                    if not fnmatch(item.a_path or "", exclude_pattern)
-                    and not fnmatch(item.b_path or "", exclude_pattern)
-                ]
+            diff_results = _get_diff_results(
+                repo, repo.commit(arguments.get("ancestor")), repo.commit(arguments.get("HEAD")), excludes
+            )
 
             try:
+                if json_format is True:
+                    diff_str = _format_diff_results_as_json(diff_results)
+                else:
+                    diff_str = _format_diff_results_as_plain_text(diff_results)
+
+                prompt = (
+                    diff_str
+                    + f"\n\n Above is the diff results for a pull request in {'the JSON format' if json_format else 'plain text'}. Please create descriptions for this pull request."
+                )
                 return types.GetPromptResult(
                     messages=[
                         types.PromptMessage(
                             role="user",
                             content=types.TextContent(
                                 type="text",
-                                text=_format_diff_results_as_json(diff_results)
-                                + "\n\n Above is the diff results for a pull request. Please create descriptions for this pull request.",
+                                text=prompt,
                             ),
                         )
                     ]
                 )
             except Exception as e:
-                raise ValueError(f"Error processing document: {str(e)}")
+                raise ValueError(f"Error generating the final prompt: {str(e)}")
         raise ValueError("Prompt implementation not found")
 
     async with stdio.stdio_server() as (read_stream, write_stream):
