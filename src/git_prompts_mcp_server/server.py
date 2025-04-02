8f9f94db439040bf6feb9b3844a5bc67458e8333
Ceshine Lee <shuanck@gmail.com>
2025-04-02 15:23:54 +0800

    Add `git-cached-diff`; Some minor adjustments

--- src/git_prompts_mcp_server/server.py
+++ src/git_prompts_mcp_server/server.py
@@ -36,6 +36,11 @@ PROMPTS = {
             ),
         ],
     ),
+    "git-cached-diff": types.Prompt(
+        name="git-cached-diff",
+        description="Generate a diff between the files in the staging area (the index) and the HEAD",
+        arguments=[],
+    ),
 }
 
 
@@ -69,7 +74,7 @@ def _format_diff_results_as_json(diff_results: list[git.Diff]) -> str:
 
 
 def _get_diff_results(
-    repo: git.Repo, source_commit: git.Commit, target_commit: git.Commit, excludes: list[str]
+    source_commit: git.Commit, target_commit: git.Commit | None, excludes: list[str]
 ) -> list[git.Diff]:
     diff_results = source_commit.diff(target_commit, create_patch=True)
 
@@ -109,7 +114,7 @@ async def run(repository: Path, excludes: list[str] = [], json_format: bool = Tr
                 raise ValueError("Arguments required")
 
             diff_results = _get_diff_results(
-                repo, repo.commit(arguments.get("ancestor")), repo.commit(arguments.get("HEAD")), excludes
+                repo.commit(arguments.get("ancestor")), repo.commit(arguments.get("HEAD")), excludes
             )
 
             try:
@@ -120,7 +125,7 @@ async def run(repository: Path, excludes: list[str] = [], json_format: bool = Tr
 
                 prompt = (
                     diff_str
-                    + f"\n\n Above is the diff results for a pull request in {'the JSON format' if json_format else 'plain text'}. Please create descriptions for this pull request."
+                    + f"\n\nAbove is the diff results for a pull request in {'the JSON format' if json_format else 'plain text'}. Please create descriptions for this pull request."
                 )
                 return types.GetPromptResult(
                     messages=[
@@ -136,14 +141,14 @@ async def run(repository: Path, excludes: list[str] = [], json_format: bool = Tr
             except Exception as e:
                 raise ValueError(f"Error generating the final prompt: {str(e)}")
         elif name == "git-diff":
+            if not arguments:
+                raise ValueError("Arguments required")
+
+            diff_results = _get_diff_results(
+                repo.commit(arguments.get("ancestor")), repo.commit(arguments.get("HEAD")), excludes
+            )
+
             try:
-                if not arguments:
-                    raise ValueError("Arguments required")
-
-                diff_results = _get_diff_results(
-                    repo, repo.commit(arguments.get("ancestor")), repo.commit(arguments.get("HEAD")), excludes
-                )
-
                 if json_format is True:
                     diff_str = _format_diff_results_as_json(diff_results)
                 else:
@@ -151,7 +156,34 @@ async def run(repository: Path, excludes: list[str] = [], json_format: bool = Tr
 
                 prompt = (
                     diff_str
-                    + f"\n\n Above is the diff results for a pull request in {'the JSON format' if json_format else 'plain text'}."
+                    + f"\n\nAbove is the diff results between HEAD and {arguments.get('ancestor')} in {'the JSON format' if json_format else 'plain text'}."
+                )
+                return types.GetPromptResult(
+                    messages=[
+                        types.PromptMessage(
+                            role="user",
+                            content=types.TextContent(
+                                type="text",
+                                text=prompt,
+                            ),
+                        )
+                    ]
+                )
+            except Exception as e:
+                raise ValueError(f"Error generating the final prompt: {str(e)}")
+
+        elif name == "git-cached-diff":
+            diff_results = _get_diff_results(repo.head.commit, None, excludes)
+
+            try:
+                if json_format is True:
+                    diff_str = _format_diff_results_as_json(diff_results)
+                else:
+                    diff_str = _format_diff_results_as_plain_text(diff_results)
+
+                prompt = (
+                    diff_str
+                    + f"\n\nAbove is the staged changes in {'the JSON format' if json_format else 'plain text'}."
                 )
                 return types.GetPromptResult(
                     messages=[
